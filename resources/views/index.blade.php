729f005a46ad73fe64b9a1620096d3acf511eaa7
farhan wahyudi <farhanwahyudi875@gmail.com>
2025-06-10 06:22:58 +0700

    Make Navbar

---
+++ resources/views/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <title>Document</title>
+</head>
+<body>
+    <nav class="flex justify-between items-center px-25 py-5">
+        <h3 class="text-2xl font-bold">Farhan</h3>
+        <div class="flex items-center gap-10 font-semibold">
+            <a href="">HOME</a>
+            <a href="">ABOUT</a>
+            <a href="">SKILLS</a>
+            <a href="">PROJECTS</a>
+            <a href="">CONTACT</a>
+        </div>
+    </nav>
+</body>
+</html>
