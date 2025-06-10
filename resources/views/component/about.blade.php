830476224c42460ea68e8a0c49c46e8b0a2559d1
farhan wahyudi <farhanwahyudi875@gmail.com>
2025-06-10 15:52:11 +0700

    About Section

---
+++ resources/views/component/about.blade.php
@@ -0,0 +1,23 @@
+<div class="py-15 px-5 sm:px-10 lg:px-20 xl:py-30">
+    <h1 class="text-3xl font-bold text-center mb-10 lg:mb-20 lg:text-4xl xl:mb-25 xl:text-5xl">ABOUT ME</h1>
+    <div class="lg:grid lg:grid-cols-2 items-center">
+        <div class="w-70 mx-auto mb-10 h-90 bg-blue-200 rounded-xl sm:w-70 lg:mb-0 xl:w-80 xl:h-100 2xl:w-90 2xl:h-120">
+            <img src="" alt="">
+        </div>
+        <div class="flex flex-col gap-10 items-center">
+            <P class="text-sm text-center md:text-base lg:border-b-2 lg:border-stone-400 lg:pb-10 2xl:text-lg">
+                <span class="font-bold text-xl lg:text-2xl">Halo!</span> Saya adalah seseorang yang sedang menekuni dunia pemrograman web secara otodidak. Meskipun berasal dari jurusan Desain Pemodelan dan Informasi Bangunan, saya menemukan ketertarikan yang kuat terhadap teknologi web dan bagaimana sebuah aplikasi bisa dibangun dari awal hingga bisa digunakan oleh banyak orang.
+            </P>
+            <div class="flex flex-col gap-5 sm:flex-row lg:flex-col xl:flex-row">
+                <div class="flex items-center gap-5 w-full h-20 shadow-sm shadow-stone-400 border-b-3 rounded-md p-3">
+                    <svg class="w-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M96 0C43 0 0 43 0 96L0 416c0 53 43 96 96 96l288 0 32 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l0-64c17.7 0 32-14.3 32-32l0-320c0-17.7-14.3-32-32-32L384 0 96 0zm0 384l256 0 0 64L96 448c-17.7 0-32-14.3-32-32s14.3-32 32-32zm32-240c0-8.8 7.2-16 16-16l192 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-192 0c-8.8 0-16-7.2-16-16zm16 48l192 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-192 0c-8.8 0-16-7.2-16-16s7.2-16 16-16z"/></svg>
+                    <p>Belajar otodidak sejak: 2023</p>
+                </div>
+                <div class="flex items-center gap-5 w-full h-20 shadow-sm shadow-stone-400 border-b-3 rounded-md p-3">
+                    <svg class="w-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M448 256A192 192 0 1 0 64 256a192 192 0 1 0 384 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256 80a80 80 0 1 0 0-160 80 80 0 1 0 0 160zm0-224a144 144 0 1 1 0 288 144 144 0 1 1 0-288zM224 256a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z"/></svg>
+                    <p>Tujuan: Menjadi web developer profesional</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/component/navbar.blade.php
+++ resources/views/component/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="fixed w-full text-white flex justify-between items-center px-5 py-5 sm:px-10 lg:px-25">
+<nav class="absolute shadow-md shadow-[#393E46] bg-[#393E46]/25 bg-opacity-0.5 w-full text-white flex justify-between items-center px-5 py-5 sm:px-10 lg:px-25">
     <h3 class="text-xl font-bold xl:text-2xl">Farhan</h3>
     <div class="hidden items-center gap-5 font-semibold sm:flex sm:text-sm md:gap-10 lg:gap-15 xl:text-base 2xl:text-lg">
         <a href="">HOME</a>
