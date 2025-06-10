9f0dea5365851686cafe125db5c51e9dc7efe333
farhan wahyudi <farhanwahyudi875@gmail.com>
2025-06-10 09:26:19 +0700

    Hero Section

---
+++ resources/views/component/navbar.blade.php
@@ -0,0 +1,10 @@
+<nav class="fixed w-full text-white flex justify-between items-center px-5 py-5 sm:px-10 lg:px-25">
+    <h3 class="text-xl font-bold xl:text-2xl">Farhan</h3>
+    <div class="hidden items-center gap-5 font-semibold sm:flex sm:text-sm md:gap-10 lg:gap-15 xl:text-base 2xl:text-lg">
+        <a href="">HOME</a>
+        <a href="">ABOUT</a>
+        <a href="">SKILLS</a>
+        <a href="">PROJECTS</a>
+        <a href="">CONTACT</a>
+    </div>
+</nav>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,15 +7,10 @@
     <title>Document</title>
 </head>
 <body>
-    <nav class="flex justify-between items-center px-25 py-5">
-        <h3 class="text-2xl font-bold">Farhan</h3>
-        <div class="flex items-center gap-10 font-semibold">
-            <a href="">HOME</a>
-            <a href="">ABOUT</a>
-            <a href="">SKILLS</a>
-            <a href="">PROJECTS</a>
-            <a href="">CONTACT</a>
-        </div>
-    </nav>
+    @include('component.navbar')
+
+    @include('component.hero')
+
+    @include('component.about')
 </body>
 </html>
